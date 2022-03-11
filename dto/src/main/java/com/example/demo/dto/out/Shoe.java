b55be51ccb34f8eaf340c4df42fb1ea6fc319109
Alexis Villareal <alex133.villareal@gmail.com>
2022-03-11 12:01:58 +0800

    Remove JsonDeserializer - no need for deserializer

--- dto/src/main/java/com/example/demo/dto/out/Shoe.java
+++ dto/src/main/java/com/example/demo/dto/out/Shoe.java
@@ -1,26 +1,18 @@
 package com.example.demo.dto.out;
 
 import com.example.demo.dto.in.ShoeFilter.Color;
-import com.example.demo.dto.out.Shoe.ShoeBuilder;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-import java.math.BigInteger;
 import lombok.Builder;
 import lombok.Value;
 
+import java.math.BigInteger;
+
 @Value
 @Builder
-@JsonDeserialize(builder = ShoeBuilder.class)
 public class Shoe {
 
-  String     name;
-  BigInteger size;
-  Color      color;
-
-  @JsonPOJOBuilder(withPrefix = "")
-  public static class ShoeBuilder {
-
-  }
+    String name;
+    BigInteger size;
+    Color color;
 
 
 }
