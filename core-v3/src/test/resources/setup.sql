422cd1c47db4bda64679bd02a44b5179fdf4314b
Alexis Villareal <alex133.villareal@gmail.com>
2022-03-16 12:40:02 +0800

    Add test

---
+++ core-v3/src/test/resources/setup.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE shoe;
+
+INSERT INTO shoe (id, name, size, quantity, color) VALUES (1,'ROCKRIDER', 42, 5, 'BLACK');
+INSERT INTO shoe (id, name, size, quantity, color) VALUES (2,'BTWIN', 44, 5, 'BLUE');
+INSERT INTO shoe (id, name, size, quantity, color) VALUES (3,'VAN RYSEL', 46, 5, 'BLACK');
